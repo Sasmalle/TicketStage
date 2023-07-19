db03ee4457a02f3385c34c8e3f4cb3d6e8f55760
Matteo Perrone <matteo.personale1@gmail.com>
2023-07-19 15:43:24 +0200

    api cliente

--- File/BackCode/TicketStage/src/main/java/com/ticketstage/integration/ClienteRest.java
+++ File/BackCode/TicketStage/src/main/java/com/ticketstage/integration/ClienteRest.java
@@ -1,5 +1,26 @@
 package com.ticketstage.integration;
 
-public class ClienteRest {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ticketstage.entities.Cliente;
+import com.ticketstage.services.ClienteService;
+
+@RestController
+@RequestMapping("api")
+public class ClienteRest {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@GetMapping("cliente")
+	public List<Cliente> getCliente(){
+		return service.getCliente();
+	}
+	
+	
 }
