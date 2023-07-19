f39bf24be39f0e42b67a2c20d2817a567a5bb9b7
Alessandro Piccolo <alessandro.piccolo30@gmail.com>
2023-07-19 02:16:16 +0200

    creato la login page

---
+++ File/FrontScript/LoginPage/script/script.js
@@ -0,0 +1,63 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const signinBtn = document.getElementById("signinBtn");
+    const codiceUtenteInput = document.getElementById("codiceUtente");
+    const rememberCheckbox = document.getElementById("rememberCheckbox");
+    const erroreTesto = document.getElementById("erroreTesto");
+
+    // Controlla se il codice utente è già stato salvato in precedenza
+    const codiceUtenteSalvato = localStorage.getItem("codiceUtente");
+    if (codiceUtenteSalvato) {
+        codiceUtenteInput.value = codiceUtenteSalvato;
+        rememberCheckbox.checked = true; // Segna automaticamente la casella di controllo "Ricorda credenziali"
+    }
+
+    signinBtn.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        const codiceUtente = codiceUtenteInput.value.trim();
+
+        if (codiceUtente === "") {
+            // Mostra il messaggio di errore e aggiungi la classe per lo stile
+            erroreTesto.textContent = "Inserisci il codice utente";
+            erroreTesto.classList.add("error-active");
+            return;
+        }
+
+        // Rimuovi il messaggio di errore e la classe di stile se il campo è valido
+        erroreTesto.classList.remove("error-active");
+
+        // Salva il codice utente in localStorage se la casella "Ricorda credenziali" è selezionata
+        if (rememberCheckbox.checked) {
+            localStorage.setItem("codiceUtente", codiceUtente);
+        } else {
+            localStorage.removeItem("codiceUtente");
+        }
+
+        // Esegui qui la chiamata API per verificare il codice utente e gestire il login
+        const url = "url-dell-api"; // Inserisci l'URL dell'API qui
+        const data = { codiceUtente: codiceUtente };
+
+        fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+        .then((response) => response.json())
+        .then((result) => {
+            if (result.valid) {
+                // Codice utente valido, esegui il login
+                // Puoi aggiungere qui il codice per il login e reindirizzare l'utente alla dashboard
+            } else {
+                // Codice utente non valido, mostra un messaggio di errore
+                erroreTesto.textContent = "Codice utente non valido";
+                erroreTesto.classList.add("error-active");
+            }
+        })
+        .catch((error) => {
+            console.error("Errore durante la chiamata API:", error);
+        });
+    });
+});
+
