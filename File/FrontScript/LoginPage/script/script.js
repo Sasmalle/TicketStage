56d23130f60c9024a324b4bee80ba95616d2f824
Savoia <andrea.savoia2811@gmail.com>
2023-07-22 12:58:05 +0200

    login concluso

--- File/FrontScript/LoginPage/script/script.js
+++ File/FrontScript/LoginPage/script/script.js
@@ -1,90 +1,66 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//   const signinBtn = document.getElementById("signinBtn");
-//   const codiceUtenteInput = document.getElementById("codiceUtente");
-//   const rememberCheckbox = document.getElementById("rememberCheckbox");
-//   const erroreTesto = document.getElementById("erroreTesto");
+document.addEventListener("DOMContentLoaded", function() {
+  const signinBtn = document.querySelector("#signinBtn");
+  const codiceUtenteInput = document.querySelector("#codiceUtente");
+  const rememberCheckbox = document.querySelector("#rememberCheckbox");
+  const erroreTesto = document.querySelector("#erroreTesto");
 
-//   // Controlla se il codice utente è già stato salvato in precedenza
+  // Controlla se il codice utente è già stato salvato in precedenza
 //   const codiceUtenteSalvato = localStorage.getItem("codiceUtente");
 //   if (codiceUtenteSalvato) {
 //       codiceUtenteInput.value = codiceUtenteSalvato;
 //       rememberCheckbox.checked = true; // Segna automaticamente la casella di controllo "Ricorda credenziali"
 //   }
 
-//   // Event listener per rimuovere il messaggio di errore quando l'utente inizia a scrivere nel campo del codice utente
-//   codiceUtenteInput.addEventListener("input", function() {
-//       erroreTesto.textContent = "";
-//       erroreTesto.classList.remove("error-active");
-//   });
+  // Event listener per rimuovere il messaggio di errore quando l'utente inizia a scrivere nel campo del codice utente
+  codiceUtenteInput.addEventListener("input", function() {
+      erroreTesto.textContent = "";
+      erroreTesto.classList.remove("error-active");
+  });
 
-//   signinBtn.addEventListener("click", function(event) {
-//       event.preventDefault();
+  signinBtn.addEventListener("click", function(event) {
+      event.preventDefault();
 
-//       const codiceUtente = codiceUtenteInput.value.trim();
+      const codiceUtente = codiceUtenteInput.value.trim();
 
-//       if (codiceUtente === "") {
-//           // Mostra il messaggio di errore e aggiungi la classe per lo stile
-//           erroreTesto.textContent = "Inserisci il codice utente";
-//           erroreTesto.classList.add("error-active");
-//       } else {
-//           // Rimuovi il messaggio di errore e la classe di stile se il campo è valido
-//           erroreTesto.textContent = "";
-//           erroreTesto.classList.remove("error-active");
+      if (codiceUtente === "") {
+          // Mostra il messaggio di errore e aggiungi la classe per lo stile
+          erroreTesto.textContent = "Inserisci il codice utente";
+          erroreTesto.classList.add("error-active");
+      } else {
+          // Rimuovi il messaggio di errore e la classe di stile se il campo è valido
+          erroreTesto.textContent = "";
+          erroreTesto.classList.remove("error-active");
 
-//           // Salva il codice utente in localStorage se la casella "Ricorda credenziali" è selezionata
-//           if (rememberCheckbox.checked) {
-//               localStorage.setItem("codiceUtente", codiceUtente);
-//           } else {
-//               localStorage.removeItem("codiceUtente");
-//           }
+          // Salva il codice utente in localStorage se la casella "Ricorda credenziali" è selezionata
+        //   if (rememberCheckbox.checked) {
+        //       localStorage.setItem("codiceUtente", codiceUtente);
+        //   } else {
+        //       localStorage.removeItem("codiceUtente");
+        //   }
 
-//         // Esegui qui la chiamata API per verificare il codice utente e gestire il login
-//     const url = "http://localhost:9005/api/cliente";
+        // Esegui qui la chiamata API per verificare il codice utente e gestire il login
+    
     
 
-//     fetch(url)
-//     .then(data => {
-//         return data.json()
-//     }) 
-//     .then(response => {
-//         // if (result.valid) {
-//         //     console.log(response);
-//         // }   
-//         // else {
-//         //     // Codice utente non valido, mostra un messaggio di errore
-//         //     erroreTesto.textContent = "Codice utente non valido";
-//         //     erroreTesto.classList.add("error-active");
-//         // }
-//         console.log(response);
-
-//     })
-    
-//     .catch((error) => {
-//     console.error("Errore durante la chiamata API:", error);
-//     });
-
-//         }
-//     });
-// });
-const url = "http://localhost:9005/api/cliente";
-function merda(){
-    fetch(url)
-    .then(data => {
-        return data.json()
-    }) 
-    .then(response => {
-        // if (result.valid) {
-        //     console.log(response);
-        // }   
-        // else {
-        //     // Codice utente non valido, mostra un messaggio di errore
-        //     erroreTesto.textContent = "Codice utente non valido";
-        //     erroreTesto.classList.add("error-active");
-        // }
-        console.log(response);
-
+    fetch("http://localhost:9005/api/cliente")
+    .then(data =>{
+        return data.json();
     })
-}
-
+    .then(clienti => {
+        console.log(clienti);
+        let codiceUtente = codiceUtenteInput.value;
+        console.log(codiceUtente);
+        let CercaCliente = clienti.find(cliente => cliente.cod_cliente == codiceUtente);
+        if(CercaCliente){
+            console.log(CercaCliente)
+            localStorage.clear();
+            localStorage.setItem("utente", JSON.stringify(CercaCliente))
+        }else{
+            erroreTesto.textContent = "Codice utente non valido";
+            erroreTesto.classList.add("error-active");
+        }
+    });
     
-    signinBtn.addEventListener("click", carica)
+        }
+    });
+});
