8cce6f62853ba9de8f98bfc614ca10a936e7c327
Alessandro Piccolo <alessandro.piccolo30@gmail.com>
2023-07-24 22:25:35 +0200

    s

---
+++ File/FrontScript/MainPage/script/script.js
@@ -0,0 +1,10 @@
+//Impostazioni del tempo di cambio foto nel carrosello
+
+   const myCarouselElement = document.querySelector('#carouselExampleFade')
+
+   const carousel = new bootstrap.Carousel(myCarouselElement, {
+   interval: 5000,
+   touch: false
+   });
+
+
