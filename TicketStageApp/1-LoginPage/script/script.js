7e50fc3e76a54c0f36ef936eea88e1a4b86f67af
Alessandro Piccolo <alessandro.piccolo30@gmail.com>
2023-07-26 08:42:27 +0200

    ss

---
+++ TicketStageApp/1-LoginPage/script/script.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function () {
+    localStorage.removeItem("utente");
+    const signinBtn = document.querySelector("#signinBtn");
+    const codiceUtenteInput = document.querySelector("#codiceUtente");
+    const rememberCheckbox = document.querySelector("#rememberCheckbox");
+    const erroreTesto = document.querySelector("#erroreTesto");
+
+    codiceUtenteInput.addEventListener("input", function () {
+        erroreTesto.textContent = "";
+        erroreTesto.classList.remove("error-active");
+    });
+
+    signinBtn.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        const codiceUtente = codiceUtenteInput.value.trim();
+
+        if (codiceUtente === "") {
+            erroreTesto.textContent = "Inserisci il codice utente";
+            erroreTesto.classList.add("error-active");
+        } else {
+            erroreTesto.textContent = "";
+            erroreTesto.classList.remove("error-active");
+
+            fetch("http://localhost:9005/api/cliente")
+                .then(data => {
+                    return data.json();
+                })
+                .then(clienti => {
+                    console.log(clienti);
+                    let codiceUtente = codiceUtenteInput.value;
+                    console.log(codiceUtente);
+                    let CercaCliente = clienti.find(cliente => cliente.cod_cliente == codiceUtente);
+                    if (CercaCliente) {
+                        console.log(CercaCliente)
+                        localStorage.clear();
+                        localStorage.setItem("utente", JSON.stringify(CercaCliente));
+                        window.location.href = "../MainPage/MainPage.html";
+                    } else {
+                        erroreTesto.textContent = "Codice utente non valido";
+                        erroreTesto.classList.add("error-active");
+                    }
+                });
+
+        }
+    });
+});
