5d146f89613163f97d7811dd3b3dc936d32ce249
andreasavoia2 <andrea.savoia2811@gmail.com>
2023-07-26 10:43:26 +0200

    js

--- TicketStageApp/3-MainPage/script/script.js
+++ TicketStageApp/3-MainPage/script/script.js
@@ -1,13 +1,13 @@
 //Impostazioni del tempo di cambio foto nel carrosello
 localStorage.removeItem("cod_spettacolo");
 localStorage.removeItem("cod_teatro");
-const myCarouselElement = document.querySelector('#carouselExampleFade')
+//const myCarouselElement = document.querySelector('#carouselExampleFade')
 const bottoni = document.querySelector('#bottoni')
 
-const carousel = new bootstrap.Carousel(myCarouselElement, {
-  interval: 5000,
-  touch: false
-});
+// const carousel = new bootstrap.Carousel(myCarouselElement, {
+//   interval: 5000,
+//   touch: false
+// });
 
 fetch("http://localhost:9005/api/teatro")
   .then(data => {
@@ -17,15 +17,19 @@ fetch("http://localhost:9005/api/teatro")
     teatro.forEach(teatri => {
       bottoni.innerHTML += 
       `
-      <div id="card1" class="card">
+
+      <div class="card" id="cardss">
       <div class="content">
         <h2 class="title">${teatri.nome}</h2>
         <p class="copy">Esplora gli spettacoli in questa sede</p>
         <button class="btn" id="${teatri.cod_teatro}" onclick="passaCod(${teatri.cod_teatro})">Acquista biglietti</button>
       </div>
-      </div>
-      </div>
-      </div> 
+    </div>
+    
+
+    </div>
+    </div>
+
       `
     });
   });
--- TicketStageApp/4-BigliettoPage/scripts/script.js
+++ TicketStageApp/4-BigliettoPage/scripts/script.js
@@ -1,4 +1,4 @@
-let carte = document.querySelector("#cards");
+let carte = document.querySelector("#carte");
 
 fetch(`http://localhost:9005/api/biglietti/bigliettoreso/2`)
     .then(data => {
@@ -7,15 +7,47 @@ fetch(`http://localhost:9005/api/biglietti/bigliettoreso/2`)
     .then(card=> {
       card.forEach(cards => {
         carte.innerHTML +=
-          `<div class="border"> 
-          <li>teatro: ${cards.teatro}</li>
-          <li>titolo: ${cards.titolo}</li>
-          <li>autore: ${cards.autore}</li>
-          <li>data della replica: ${cards.data_replica}</li>
-          <li>quantità: ${cards.quantita}</li>
-          <li>cognome: ${cards.cognome}</li>
-          <li>nome: ${cards.nome}</li>
-          </div>`
+          `
+          
+  <div class="ticket">
+    <div class="stub">
+      <div class="top">
+        <span class="admit">Admit</span>
+        <span class="line"></span>
+        <span class="num">
+          cliente
+          <span>${cards.cognome} ${cards.nome}</span>
+        </span>
+      </div>
+      <div class="number">${cards.quantita}</div>
+      <div class="invite">
+        ${cards.teatro}
+      </div>
+    </div>
+    <div class="check">
+      <div class="big">
+        ${cards.titolo}
+      </div>
+      <div class="number">#1</div>
+      <div class="info">
+        <section>
+          <div class="title">Date</div>
+          <div>${cards.data_replica}</div>
+        </section>
+        <section>
+          <div class="title">Issued By</div>
+          <div>Ampersand</div>
+        </section>
+        <section>
+          <div class="title">Autore</div>
+          <div>${cards.autore}</div>
+        </section>
+      </div>
+    </div>
+  
+          
+  `
+          
       });
     });
 
