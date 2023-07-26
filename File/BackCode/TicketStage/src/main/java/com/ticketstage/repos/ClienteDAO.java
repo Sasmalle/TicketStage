dd47537d40fb91e4a9fc47096c57776604c7d51b
andreasavoia2 <andrea.savoia2811@gmail.com>
2023-07-26 10:09:28 +0200

    api creata

--- File/BackCode/TicketStage/src/main/java/com/ticketstage/repos/ClienteDAO.java
+++
@@ -1,9 +0,0 @@
-package com.ticketstage.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ticketstage.entities.Cliente;
-
-public interface ClienteDAO extends JpaRepository<Cliente, Integer> {
-
-}
